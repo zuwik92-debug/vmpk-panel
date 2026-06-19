923d6c2528c4772b7e0d54d3ab18aeaf05ae3ca4
zuwik92-debug <zuwik92@gmail.com>
2026-06-19 15:25:08 +0200

    fix: ostateczny kompletny kod projektu bez błędów

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serwowanie plików statycznych z folderu public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Obsługa routingu - przekierowanie na stronę główną
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
